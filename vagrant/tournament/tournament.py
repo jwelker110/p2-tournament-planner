d75d8fcd65f374363e0603f50d5e3779783e7c97
Jon Welker <JWelker110@gmail.com>
2015-03-03 14:07:17 -0600

    Initial Commit

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -13,14 +13,33 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    query = "DELETE FROM matches;"
+    cursor.execute(query)
+    db.commit()
+    db.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    query = "DELETE FROM players;"
+    cursor.execute(query)
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    cursor = db.cursor()
+    query = "SELECT count(id) FROM players;"
+    cursor.execute(query)
+    players = cursor.fetchone()[0]
+    db.close()
+    return players
 
 
 def registerPlayer(name):
@@ -32,6 +51,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO players (name) VALUES(%s);", (name,))
+    db.commit()
+    db.close()
 
 
 def playerStandings():
@@ -47,6 +71,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM standings ORDER BY wins DESC;")
+    standings = cursor.fetchall()
+    db.close()
+    return standings
 
 
 def reportMatch(winner, loser):
@@ -56,7 +86,11 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    DB = connect()
+    cursor = DB.cursor()
+    cursor.execute("INSERT INTO matches (win_id, lose_id) VALUES(%s, %s);", (winner, loser))
+    DB.commit()
+    DB.close()
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
@@ -74,4 +108,49 @@ def swissPairings():
         name2: the second player's name
     """
 
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT win_id, lose_id FROM matches;")
+    matches = cursor.fetchall()
+    db.close()
 
+    standings = playerStandings()
+    pairings = []
+    length = len(standings)
+
+    # start from the highest ranked player
+    for i in range(length - 1):
+        if standings[i] is None:
+            continue
+        # set first opponent of pair, (id, name)
+        first_opponent = [standings[i][0], standings[i][1]]
+        # removing opponent from list of possible opponents
+        standings[i] = None
+
+        # start from player adjacent in rank to first opponent
+        for j in range(i + 1, length, 1):
+            # make sure an opponent is at this index
+            if standings[j] is None:
+                continue
+            # set second opponent of pair, (id, name)
+            second_opponent = [standings[j][0], standings[j][1]]
+
+            # if players haven't played together yet, pair is accepted
+            if not playersAlreadyMatched([first_opponent, second_opponent], matches):
+                # print("i = (%d) j = (%d)" % (i, j))
+                # add pair to the pairings list, (id1, name1, id2, name2)
+                pairings.append((first_opponent[0], first_opponent[1], second_opponent[0], second_opponent[1]))
+                # remove second opponent from list of possible opponents
+                standings[j] = None
+                break
+
+    return pairings
+
+
+def playersAlreadyMatched(pairing, matches):
+    for match in matches:
+        # check for previous match with these players
+        if (match[0] == pairing[0][0] and match[1] == pairing[1][0]) or \
+                (match[0] == pairing[1][0] and match[1] == pairing[0][0]):
+            return True
+    return False
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,38 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Clear DB to start fresh
+DROP VIEW standings;
+DROP TABLE matches;
+DROP TABLE players;
 
+CREATE TABLE players(
+  id serial PRIMARY KEY ,
+  name text
+);
+
+CREATE TABLE matches(
+  id serial PRIMARY KEY,
+  win_id INT REFERENCES players (id),
+  lose_id INT REFERENCES players (id),
+  CHECK (win_id != lose_id),
+  UNIQUE (win_id, lose_id)
+
+);
+
+CREATE VIEW standings (id, name, wins, matches) AS
+  SELECT p.id, p.name, count(m.win_id), player_matches.num_matches
+  FROM players as p
+    LEFT JOIN matches as m
+  ON p.id=m.win_id
+    LEFT JOIN
+    (SELECT p.id, count(m) as num_matches FROM players as p
+      LEFT JOIN matches as m
+      ON p.id=m.win_id OR p.id = m.lose_id GROUP BY p.id) as player_matches
+      ON p.id=player_matches.id
+  GROUP BY p.id, player_matches.num_matches;
+
+-- Ensure DB is empty by displaying tables and views
+SELECT * FROM standings;
+SELECT * FROM matches;
+SELECT * FROM players;
